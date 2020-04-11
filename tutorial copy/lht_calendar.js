d6a66f258f0879966b41a9afb410ecc3b27d7323
xemerson <45703276+xiaemerson@users.noreply.github.com>
2020-04-11 14:52:03 -0700

    2

--- tutorial copy/lht_calendar.js
+++ tutorial copy/lht_calendar.js
@@ -5,13 +5,13 @@
    Tutorial 10
    Tutorial Case
 
-   Author: 
-   Date:  
-
+   Author: Emerson Xia
+   Date: 04-11-2020
+   
    Filename:   lht_calendar.js  
 
    Function List:
-   createCalendar(calDate)
+   createCalendar(calDate)a
       Creates the calendar table for the month specified in the
       calDate parameter. The current date is highlighted in 
       the table.
@@ -28,5 +28,34 @@
    calDays(calDate)
       Writes the daily rows in the calendar table, highlighting calDate
 	
-*/
+   /* Set the date displayed in the calendar */
+   var thisDay = new Date("August 24, 2018");
+
+   /* Write the calendar to the element with th id "calendar" */
+   document.getElementById("calendar").innerHTML = createCalendar(thisDay);
+
+   /* Function to generate the calendar table */
+   function createCalendar(calDate) {
+      var calendarHTML = "<table id='calendar_table'>";
+      calendarHTML += calCaption(calDate);
+      calendarHTML += "</table>";
+      return calendarHTML;
+   }
+
+   /* Function to write the calendar caption */
+   function calCaption(calDate) {
+      // monthName array contains the list of month names
+      var monthName = ["January", "Febuary", "March", "April", 
+                       "May", "June", "July", "August", "September",
+                       "October", "November", "December"];
+
+      //Determine the current month
+      var thisMonth = calDate.getMonth();
+
+      //Determine the current year
+      var thisYear = calDate.getFullYear();
+
+      //Write the caption
+      return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+   }
 
