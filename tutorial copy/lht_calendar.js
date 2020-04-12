79a622a6b2fbf57f329f088eca2e20f6e9bc8b16
xemerson <45703276+xiaemerson@users.noreply.github.com>
2020-04-12 15:21:13 -0700

    b

--- tutorial copy/lht_calendar.js
+++ tutorial copy/lht_calendar.js
@@ -29,95 +29,97 @@
       Writes the daily rows in the calendar table, highlighting calDate
 	
    /* Set the date displayed in the calendar */
-   var thisDay = new Date("August 24, 2018");
+var thisDay = new Date();
 
-   /* Write the calendar to the element with th id "calendar" */
-   document.getElementById("calendar").innerHTML = createCalendar(thisDay);
+/* Write the calender to the element with the id "calendar"*/
+document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 
-   /* Function to generate the calendar table */
-   function createCalendar(calDate) {
-      var calendarHTML = "<table id='calendar_table'>";
-      calendarHTML += calCaption(calDate);
-      calendarHTML += calWeekdayRow();
-      calendarHTML += calDays(calDate);
-      calendarHTML += "</table>";
-      return calendarHTML;
-   }
+/*Function to generate the calendar table*/
+function createCalendar(calDate) {
+    var calendarHTML = "<table id='calendar_table'>";
+    calendarHTML += calCaption(calDate);
+    calendarHTML += calWeekdayRow();
+    calendarHTML += calDays(calDate);
+    calendarHTML += "</table>";
+    return calendarHTML;
+}
+/*Function to wrtie calendar caption*/
+function calCaption(calDate) {
+    // monthName arry contains the list of month names 
+    var monthName = ["January", "February", "March", "April", "May", "June" , "July", "August", "September", "October", "November", "December"];
 
-   /* Function to write the calendar caption */
-   function calCaption(calDate) {
-      // monthName array contains the list of month names
-      var monthName = ["January", "Febuary", "March", "April", 
-                       "May", "June", "July", "August", "September",
-                       "October", "November", "December"];
+    //Determine the current month
+    var thisMonth = calDate.getMonth();
 
-      //Determine the current month
-      var thisMonth = calDate.getMonth();
+    //Determine the current year
+    var thisYear = calDate.getFullYear();
 
-      //Determine the current year
-      var thisYear = calDate.getFullYear();
+    //Write the caption 
+    return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
 
-      //Write the caption
-      return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
-   }
-   /* Function to write a table row of weekday abbreviations */
-   function calWeekdayRow() {
-      // Array of weekday abbrevations
-      var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-      var rowHTML = "<tr>";
-      // Look through the dayName array
-      for (var i = 0; i < dayName.length; i++) {
-         rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
-      }
-      
-      rowHTML += "</tr>";
-      return rowHTML;  
-   }
+// Function to wirte a table row if weekday abbreviations
+function calWeekdayRow() {
+    //Array of weekday abbreviations
+    var dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    var rowHTML = "<tr>";
+    
+    //Loop Through the dayName array
+    for (var i = 0; i < dayName.length; i++) {
+        rowHTML += "<th class='calender_weekdays'>" + dayName[i] +"</th>";
+    }//for loop close
 
-   /* Function to calculate the number of days in the month */
-   function daysinMonth(calDate) {
-      //arrays of days in each month
-      var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
+    //Closing Table Row tag
+    rowHTML += "</tr>";
+    return rowHTML;
+}//function close
 
-      //Extract the four digit year and month value
-      var thisYear = calDate.getFullYear();
-      var thisDay = cal.Date.getMonth();
+//Function to calculate the number of days in the month
+function daysInMonth(calDate) {
+    //Array of days in each month
+    var dayCount = [31,28,31,30,31,30,31,31,30,31,30,31];
 
-      //Revise the days in Febuary for leap years
-      if (thisYear % 4 == 0) {
-         if ((thisYear % 100 != 0) || (thisYear % 400 == 0)) {
-         dayCount[1] = 29;
-         }  
-      }
+    //Extract the four digit year and month value
+    var thisYear = calDate.getFullYear();
+    var thisMonth = calDate.getMonth();
+    
+    //Revise the day in February for leap years
+    if (thisYear % 4 === 0) {
+        dayCount[1] = 29;
+        if ((thisYear % 100 != 0) || (thisYear % 400 ===0)) {
+            dayCount[1] = 29;
+        }//Nested if
+    }//Main if
 
-      //Return the number of days for the current month
-      return dayCount[thisMonth]
-   }
+    //Return the number of days for the  current month
+    return dayCount[thisMonth];
+}//end main function
 
-   /* Function to write table rows for each day of the month */
-   function calDays(calDate) {
-      // Determine the starting day of the month
-      var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
-      var weekDay = day.getDay();
+//Function to write table rows for each day of the month
+function calDays(calDate) {
+    //Determine the staring day of the month
+    var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+    var weekDay = day.getDay(); 
+    //Write blank cells preceding the staring day
+    var htmlCode = "<tr>";
+    for (var i = 0; i < weekDay; i++) {
+        htmlCode += "<td></td>"
+    }
+    //Write cells for each day of the month
+    var totalDays = daysInMonth(calDate);
+    var highlighDay = calDate.getDate();
+    for (var i = 1; i <= totalDays; i++) {
+        day.setDate(i);
+        weekDay = day.getDay();
 
-      //Write blank cells preceeding the starting day
-      var htmlCode = "<tr>";
-      for (var i = 0; 1 < weekDay; i++) {
-         htmlCode += "<td></td>";
-      }
-      //Write cells for each day of the month
-      var totalDays = daysinMonth(calDate);
-
-      for (var i = 1; i <= totalDays; i++) {
-         day.setDay(i);
-         weekDay = day.getDay();
-
-         if (weekDay === 0) htmlCode += "<tr>";
-         htmlCode += "<td class='calendar_dates'>" + i + "</td>";
-         if (weekDay === 6) htmlCode += "</tr>";
-      }
-
-      return htmlCode
-               
-   }
-
+        if (weekDay === 0) htmlCode += "<tr>";
+        if (i === highlighDay) {
+            htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + dayEvent[i] +  "</td>";
+        } else {
+            htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+        }
+        if (weekDay === 6) htmlCode += "</tr>";
+    }
+    //Return table rows and table data to make a calendar
+    return htmlCode;
+}
