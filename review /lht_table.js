166374799d859fe01559435488334fb29ab90700
xemerson <45703276+xiaemerson@users.noreply.github.com>
2020-04-16 16:55:24 -0700

    b

--- review /lht_table.js
+++ review /lht_table.js
@@ -4,17 +4,16 @@
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 10
    Review Assignment
-
    Author: Emerson Xia
-   Date: 04/16/2020   
-
+   Date:   04/16/2020
 	
 */
 
-//setting the date displayed in the calendar
-var thisDay = new Date("August 30,2020");
 
-//HTML code for event list table
+//Setting the date displayed in the calendar
+var thisDay = new Date("August 30, 2018");
+
+//Writing the HTML code for the event list table
 var tableHTML = "<table id='eventTable'>";
 tableHTML += "<caption>Upcoming Events</caption>";
 tableHTML += "<tr><th>Date</th><th>Event</th><th>Price</th></tr>";
@@ -22,13 +21,13 @@ tableHTML += "<tr><th>Date</th><th>Event</th><th>Price</th></tr>";
 //Setting the end date for the event list two weeks from the current date
 var endDate = new Date(thisDay.getTime() + 14*24*60*60*1000);
 
-//looping through eventDates array
+//Looping through the eventDates array
 for (var i = 0; i < eventDates.length; i++) {
-	var eventDate = new Date(eventDates[i]);
-	var eventDay = eventDate.toDateString();
-	var eventTime = eventDate.toLocalTimeString();
-
-	/* If the event date is within the two-week window, display it on the page */
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleTimeString();
+   
+   //If the event date is within the two-week window, display it on the page
    if (thisDay <= eventDate && eventDate <= endDate) {
       tableHTML += "<tr>";
       tableHTML += "<td>" + eventDay + " @ " + eventTime + "</td>";
@@ -40,6 +39,5 @@ for (var i = 0; i < eventDates.length; i++) {
    
 tableHTML += "</table>";
 
-/* Write the HTML code into the eventList box */
+//Writing the HTML code into the eventList box
 document.getElementById("eventList").innerHTML = tableHTML;
-
